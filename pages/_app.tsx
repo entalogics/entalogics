12f96a157ab9a5a57d388038824775fcdd05f932
wiredbuilders <wiredbuilders@gmail.com>
2025-07-23 15:08:05 +0500

    chore: add Vercel Analytics to app layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import '../index.css';
 import { ThemeProvider } from '../src/context/ThemeContext';
 import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/next';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   );
 } 
